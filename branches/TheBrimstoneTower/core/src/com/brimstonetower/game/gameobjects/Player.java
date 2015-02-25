ffb0b4c65aa7507be8d8e3b8d4e2eb2430055c6a
kralle@gmail.com <kralle@gmail.com@640db188-a9b9-22b8-ecae-6170c3a81e70>
2015-02-25 17:02:56 +0000

    starting with wrong weapon :D

--- branches/TheBrimstoneTower/core/src/com/brimstonetower/game/gameobjects/Player.java
+++ branches/TheBrimstoneTower/core/src/com/brimstonetower/game/gameobjects/Player.java
@@ -50,7 +50,7 @@ public class Player extends GameCharacter
     public Player(String name)
     {
         super(name, _startStr, 5, _startHp, AssetManager.getTextureRegion("mainHeroesWithBorder", "playerType2", DungeonMap.TileSize, DungeonMap.TileSize));
-        super.equip(ItemManager.getWeapon("Steel Throwing axe"));//Player starts with sword
+        super.equip(ItemManager.getWeapon("Steel Short Sword"));//Player starts with sword
         super.equip(ItemManager.getArmor("Noble Clothes"));//Player starts with some clothes
         _equippedWeapon.identify();
         _equippedArmor.identify();
