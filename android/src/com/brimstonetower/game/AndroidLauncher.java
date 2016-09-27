663f3b3cf980882471008e8b84385534d4b1c40f
Kristian <kralle@gmail.com>
2016-09-27 11:09:56 +0200

    Removed invalid android launcher

--- android/src/com/brimstonetower/game/AndroidLauncher.java
+++ android/src/com/brimstonetower/game/AndroidLauncher.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.brimstonetower.game.TheBrimstoneTowerGame;
 
 public class AndroidLauncher extends AndroidApplication {
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
